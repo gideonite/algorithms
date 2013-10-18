e55e9bae0c4948892cd435ce23f994331a1e2ce5
Gideon Dresdner <gideond@gmail.com>
2013-10-18 12:27:32 -0400

    bubble sort works

--- src/algorithms/bubble_sort.clj
+++ src/algorithms/bubble_sort.clj
@@ -1,23 +1,29 @@
 (ns algorithms.bubble-sort)
 
-(defn bubble-step [coll is-sorted less?]
-  (if (second coll)
+;; inspired and largely taken whole sheet and cloth from
+;; http://rosettacode.org/wiki/Sorting_algorithms/Bubble_sort#Clojure
+
+(defn bubble-step
+  [coll was-changed less?]
+  (if (not (second coll))     ;; count > 1 without counting the list
+    [coll (not was-changed)]
     (let [[x1 x2 & xs] coll
-          is-less-than (less? x1 x2)
-          smaller (if is-less-than x1 x2)
-          larger (if is-less-than x2 x1)
-          is-changed (or is-sorted (not is-less-than))
-          [sorted is-sorted] (bubble-step (cons larger xs) is-changed less?)]
-      [(cons smaller sorted) is-sorted])
-    [coll is-sorted]))
+          first-is-smaller (less? x1 x2)
+          is-changed (or was-changed (not first-is-smaller))
+          [smaller larger] (if first-is-smaller [x1 x2] [x2 x1])
+          [result is-sorted] (bubble-step (cons larger xs) is-changed less?)]
+      [(cons smaller result) is-sorted])))
 
-(defn bubble-sort [coll less?]
-  (loop [[coll is-sorted] [coll false]]
-    (if is-sorted
-      coll
-      (recur (bubble-step coll is-sorted less?)))))
+(defn bubble-sort
+  [coll less?]
+  (let [step #(bubble-step %1 %2 less?)]
+    (loop [[coll is-sorted] [coll false]]
+      (if is-sorted
+        coll
+        (recur (step coll is-sorted))))))
 
-(bubble-step [40 32 1 300] false <)
-(bubble-step [1 2 3] false <)
+(comment
+  (bubble-step [40 32 1 300] false <)
+  (bubble-sort [40 32 1 300] <)
 
-;(bubble-sort [40 32 1 300] <)
+  (bubble-sort [123123 39 5 4 40 12 32 2 1 300] <))
