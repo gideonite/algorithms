4be650edda9db4b42c5c7b7e92cdae810f90372e
Gideon Dresdner <gideond@gmail.com>
2013-10-22 21:47:06 -0400

    cons cells for stacks, two stacks for queues, easy

--- src/algorithms/priority_map.clj
+++ src/algorithms/priority_map.clj
@@ -1,49 +1,64 @@
 (ns algorithms.priority-map)
 
 (defn stack
-  "a stack is a {:head :rest}"
+  "a stack is just a cons list"
   [& xs]
-  (let [x1 (first xs)
-        xs (rest xs)]
-    (if (not x1)
-      {:head nil :rest nil}
-      {:head x1 :rest xs})))
+  xs)
 
 (defn push-stack
+  "just does `cons`"
   [s item]
-  {:head item
-   :rest (cons (:head s) (:rest s))})
+  (cons item s))
 
 (defn pop-stack
+  "stack -> stack"
   [s]
-  (let [r (:rest s)]
-    (list (:head s)
-          {:head (first r)
-           :rest (rest r)})))
+  (pop (into '() s)))
+
+(defn peek-stack
+  [s]
+  "stack -> item"
+  (peek s))
 
 (defn empty-stack?
+  "stack -> boolean"
   [s]
   (nil? (:head s)))
 
+ ;; A queue is a map with a pulling stack and a pushing stack:
+ ;; {:push stack :pull stack}
+
 (defn queue
-  "returns an unbalanced queue (i.e. [stack nil])"
+  "returns an unbalanced queue"
   [& items]
-  [(apply stack items) (stack)])
+  {:push (apply stack items) :pull (stack)})
 
 (defn balance-queue
-  "flip the stack if reverse is empty, otherwise do nothing."
+  "queue -> queue with nil pushing stack
+  lazily attaches the pushing stack to the end of the pulling stack"
   [q]
-  (let [[f r] q]
-    (if (empty-stack? r)
-      [(stack)
-       (apply stack (reverse (cons (:head f) (:rest f))))]
-      [f r])))
+  {:push nil
+   :pull (reverse (concat (:push q) (:pull q)))})
+
+(defn balanced?
+  [q]
+  (not (nil? (:pull q))))
 
 (defn pop-queue
+  "queue -> queue"
   [q]
-  (let [[f r] (balance-queue q)]
-    (list (:head r) )
-    ))
+  (let [b? (balanced? q)
+        q (if b? q (balance-queue q))]
+    {:push (:push q)
+     :pull (pop (:pull q))}))
+
+(defn push-queue
+  "queue item -> queue"
+  [q item]
+  {:push (cons item (:push q))
+   :pull (:pull q)})
+
+;; TODO peek queue
 
 (comment
   (stack 1 2 3)
@@ -55,10 +70,11 @@
   (pop-stack (stack 1 2 3))
   (pop-stack (push-stack (stack 1) 2))
 
+
   (queue 1 2 3)
-  (balance-queue (queue 1 2 3))
+  (:pull (balance-queue (queue 1 2 3))) 
   (balance-queue (balance-queue (queue 1 2 3))) ;nilpotent
 
-  (pop-queue (queue 1 2 3))
+  (pop-queue (push-queue (queue 1) 5))
 
   )
