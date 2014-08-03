6ee1e1afc20a38286d11c0ef746e6dc67ecef24c
Gideon Dresdner <gideond@gmail.com>
2014-08-03 18:37:04 -0400

    Compiles with an identity place holder for resize. Does it run though???

--- src/algorithms/hash_map.clj
+++ src/algorithms/hash_map.clj
@@ -3,22 +3,32 @@
 ;; A hash map provides the following basic interface:
 ;; - get O(1)
 ;; - put O(1)
-
+;;
 ;; I am going to use a hashing function to map keys to indices.
 ;; As a backing store, I'm going to use a vector. A hashmap, is
 ;; precisely these two things.
-
+;;
 ;; Represent an empty location in the backing array as `[nil nil]`.
 
-(def the-empty-map (into [] (repeat 8 [nil nil])))
+(def hashnil (gensym "gideonhashmap"))
+
+(def the-empty-map (into [] (repeat 8 [hashnil hashnil])))
+
+(defn- dump
+  [hmap k v]
+  (assoc hmap (mod (hash k) (count hmap)) [k v]))
+
+(defn find-first
+  [pred coll]
+  (first (filter pred coll)))
 
 (defn- lookup
   [hmap k]
   (hmap (mod (hash k) (count hmap))))
 
-(defn- dump
-  [hmap k v]
-  (assoc hmap (mod (hash k) (count hmap)) [k v]))
+(defn resize
+  [hmap]
+  hmap)
 
 (defn hash-put
   "Open addressing means that to resolve collisions you
@@ -28,8 +38,11 @@
     (if (= curr-key k)
       (dump hmap k v)
       ;; seek an empty spot
-      )
-    ))
+      (let [index-value (find-first #(= hashnil (first %))
+                                  (drop (mod (hash k) (count hmap)) hmap))]
+        (if (nil? index-value)
+          (hash-put (resize hmap) k v)
+          (assoc hmap (first index-value) v))))))
 
 (def my-hash-map the-empty-map)
 ;; TODO overwrite the default print method to print non nil key-value pairs.
